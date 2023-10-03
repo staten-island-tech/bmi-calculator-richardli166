4168271b0af0f8506cc3b6f2791881ffa99fa7ce
richardli166 <water.richard.li@gmail.com>
2023-10-03 09:22:49 -0400

    ewgrhg

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//declare and define function, input output machines 
+/* //declare and define function, input output machines 
 //we can pass in arguments or parameters
 function test(message) {
     console.log(message);
@@ -12,7 +12,7 @@ const string = "String";
 test(string);
 
 //Number/Integer
-const integer = 98;
+const integer = 99;
 test(integer);
 
 //Boolean true or false
@@ -20,10 +20,27 @@ const boolean = true;
 test(boolean);
 
 function add(number, number2){
-    console.log(
-    number+number2
-    )
+    console.log(number+number2)
 }
+//need to convert a String to a Number
 let a = Number(prompt("enter a number"));
 let b = Number(prompt("enter another number"));
 add(a, b);
+
+function add(x, y){
+    return x + y;
+}
+const sum = add
+ */
+function bmicalculator(height, weight){
+    return ((weight/(height*height))*10000);
+}
+let name = (prompt("Enter your Name"))
+let weight = Number(prompt("Enter your weight in kg"))
+let height = Number(prompt("Enter your height in cm"));
+const user = bmicalculator(height, weight)
+console.log(name)
+console.log(user)
+function print(value){
+    document.getElementById('log').innerHTML += value;
+}
